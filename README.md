cd43e4c9e0fc82c9442b7e036d6b155e98dc2f16
Guilherme Sartori <guilherme.sartori@orys.com.br>
2023-09-22 11:31:24 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# DASHBOARD - DESAFIO TIKTOK 01
+
+Olá, frontend. Que bom te ver por aqui.
+
+Para rodar e testar essa tela de conceito você vai precisar seguir o passo a passo:
+
+## 1. clone o repositório para o seu computador:
+
+`$ git clone https://github.com/guisartori/dashboard-users-as-clients.git`
+
+## 2. abra o arquivo index.html:
+
+Dois cliques diretos no arquivo.
+
+Pronto. Você está rodando ele localmente na sua máquina.
+
+Abraço galera! Vejo vocês em breve.
---
+++ script.js
@@ -0,0 +1,34 @@
+const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+      datasets: [{
+        label: 'Views',
+        data: [12, 19, 3, 5, 2, 3, 6],
+        borderWidth: 1,
+      },
+    {
+        label: 'Follows',
+        data: [1, 2, 3, 3, 3, 4, 6],
+        borderWidth: 1
+    }]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          title: {
+            text: ['10k', '20k', '30k', '40k', '50k']
+          }
+        }
+      }
+    }
+  });
